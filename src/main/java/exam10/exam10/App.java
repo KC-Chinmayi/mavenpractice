1090e8776de4f3efa7c48815f36684c29a8a7fa1
miniproject <kcchinmayi@gmail.com>
2025-05-27 20:01:39 +0530

    second commit

---
+++ src/main/java/exam10/exam10/App.java
@@ -0,0 +1,13 @@
+package exam10.exam10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
